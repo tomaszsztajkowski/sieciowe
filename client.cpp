1fb1b5d8d32a36a2742f555bbb7767eaee91497a
tomaszsztajkowski@gmail.com <46276902+tomaszsztajkowski@users.noreply.github.com>
2022-03-23 07:49:14 +0100

    quit and infinite messages

--- client.cpp
+++ client.cpp
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <cstdio>
 #include <sys/socket.h>
-#include <cstdlib>
 #include <netinet/in.h>
 #include <cstring>
 #include <arpa/inet.h>
@@ -10,10 +9,9 @@
 int main() {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    char buffer_client[1024] = {0};
+    char buffer_server[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
@@ -22,20 +20,25 @@ int main() {
     serv_addr.sin_port = htons(8080);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+
+    while (strcmp(buffer_client, "quit") != 0) {
+        std::cout << "Enter message: \n";
+        std::cin >> buffer_client;
+
+        send(sock, buffer_client, strlen(buffer_client), 0);
+        valread = read(sock, buffer_server, 1024);
+        std::cout << buffer_server << '\n';
+        memset(buffer_server, 0, valread);
+    }
+
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -12,7 +12,6 @@ int main()
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -50,9 +49,13 @@ int main()
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    while(strcmp(buffer, "quit") != 0){
+        memset(buffer, 0, strlen(buffer));
+        valread = read(new_socket, buffer, 1024);
+        printf("%s\n", buffer);
+        send(new_socket, buffer, strlen(buffer), 0);
+    }
+
     return 0;
 }
